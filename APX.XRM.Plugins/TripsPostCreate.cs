bd21c16c8bc670f9c25661aaa0774d2954b4f7e5
Anne Pessoa <annepessoa@gmail.com>
2026-01-13 21:24:11 +0000

    Update trip name assignment logic in TripsPostCreate

--- APX.XRM.Plugins/TripsPostCreate.cs
+++ APX.XRM.Plugins/TripsPostCreate.cs
@@ -16,11 +16,21 @@ namespace APX.XRM.Plugins
             var trip = (apx_Trips)ctx.PostImage;
             if (trip.apx_Name == null)
             {
-                var location = ctx.Service.GetEntity<APX_Location>(trip.apx_Location, new ColumnSet(APX_Location.Fields.apx_Name));
-
+                var newName = "";
+                if (trip.apx_Location == null)
+                {
+                    newName = $"New Trip";
+                }
+                else
+                {
+                    var location = ctx.Service.GetEntity<APX_Location>(trip.apx_Location, new ColumnSet(APX_Location.Fields.apx_Name));
+                    newName = $"Trip to {location?.apx_Name}";
+                }
                 var tripToUpdate = new apx_Trips(trip.Id);
-                tripToUpdate.apx_Name = $"Trip to {location?.apx_Name} on {trip.apx_Start}";
+                tripToUpdate.apx_Name = newName;
                 ctx.Service.Update(tripToUpdate);
+
+                ctx.Trace($"Trip {trip.Id} name updated to {newName}");
             }
         }
     }
